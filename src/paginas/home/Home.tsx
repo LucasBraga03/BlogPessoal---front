623056cf20b462e6c0843c5cd228e2bd3faa4acf
Lucas Silva Braga <lucas.sbraga14@gmail.com>
2022-02-25 16:37:05 -0300

    correção de rotas

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import {Typography, Box, Grid, Button} from '@material-ui/core';
+import { Typography, Box, Grid, Button } from '@material-ui/core';
 import TabPostagem from '../../components/postagens/tabpostagem/TabPostagem';
 import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
 import './Home.css';
@@ -11,14 +11,14 @@ function Home() {
 
     let history = useHistory();
     const [token, setToken] = useLocalStorage('token');
-    
+
     useEffect(() => {
-      if (token == "") {
-          alert("Você precisa estar logado")
-          history.push("/login")
-  
-      }
-  }, [token])
+        if (token == "") {
+            alert("Você precisa estar logado")
+            history.push("/login")
+
+        }
+    }, [token])
 
     return (
         <Grid container className="back2">
@@ -30,6 +30,14 @@ function Home() {
                             <Typography className='bemvindo' variant="h3" gutterBottom color="textPrimary" component="h3" align="center" >Seja bem vindo(a)!</Typography>
                             <Typography className='expresse' variant="h5" gutterBottom color="textPrimary" component="h5" >expresse aqui os seus pensamentos e opiniões!</Typography>
                         </Box>
+
+                        <Box display="flex" justifyContent="center">
+                            <Box marginRight={1}>
+                                <ModalPostagem />
+                            </Box>
+                            <Button variant="outlined" className='botao'>Ver Postagens</Button>
+                        </Box>
+
                         <Box>
                             <Button className='postagem'>Ver Postagens</Button>
                         </Box>
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://bloggeneration.herokuapp.com'
+    baseURL: 'https://bloglb.herokuapp.com'
 })
     export const cadastroUsuario = async(url: any,dados: any,setDado: any) => { 
         const resposta = await api.post(url,dados)
